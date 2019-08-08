52509b491c33af1b2bae856c98fc5501e4883cc4
WozniakMichal <52409805+WozniakMichal@users.noreply.github.com>
2019-08-08 13:55:00 +0200

    Feature/youg 12/form module (#66)
    * [!WIP] form module block
    
    * [!WIP] form module block
    
    * refactor of templates

--- streams/blocks.py
+++ streams/blocks.py
@@ -7,7 +7,7 @@ from wagtail.images.blocks import ImageChooserBlock
 from wagtail.documents.blocks import DocumentChooserBlock
 
 from wagtailmedia.blocks import AbstractMediaChooserBlock
-
+from wagtailstreamforms.blocks import WagtailFormBlock
 
 # wont be used. Everything on video_block template
 
@@ -507,3 +507,43 @@ class TableModuleBlock(blocks.StructBlock):
         template = "streams/table_block.html"
         icon = "list-ul"
         label = "Table module"
+
+
+""" Form module blocks """
+
+
+class FormPageBlock(blocks.StructBlock):
+    page = blocks.PageChooserBlock(required=True, help_text="choose page")
+    link_tab_chooser = LinkTabChooserBlock(required=True, help_text="choose either open page on new or current tab")
+
+    class Meta:
+        icon = "site"
+
+
+class FormDocumentBlock(blocks.StructBlock):
+    document = DocumentChooserBlock(required=True, help_text="choose file eg. PDF")
+
+    class Meta:
+        icon = "doc-full"
+
+
+class FormPageDocBlock(blocks.StreamBlock):
+    internal_page = FormPageBlock()
+    doc = FormDocumentBlock()
+
+    class Meta:
+        max_num = 1
+        icon = "link"
+
+
+class FormLink(blocks.StructBlock):
+
+    link_text = blocks.CharBlock(required=True, max_length=256, help_text="add text")
+    link = FormPageDocBlock(required=True, help_text="choose page or document")
+    error_message = blocks.CharBlock(required=True, max_length=256, help_text="add message")
+    success_message = blocks.CharBlock(required=True, max_length=256, help_text="add message")
+    sub_copy = blocks.CharBlock(required=True, max_length=256, help_text="add sub copy")
+
+
+class FormModuleBlock(WagtailFormBlock):
+    link_data = FormLink(required=True)
