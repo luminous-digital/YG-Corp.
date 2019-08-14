7d3fb244a4334f50676736936d064e1589b0a3e2
YMerritt <53304940+YMerritt@users.noreply.github.com>
2019-08-14 13:19:47 +0200

    Feature/youg 28/timeline module (#90)
    * wip timeline module
    
    * timeline module
    
    * timeline module

--- flex/models.py
+++ flex/models.py
@@ -38,6 +38,7 @@ class AbstractFlexPage(Page):
             ('form_module_block', blocks.FormModuleBlock()),
             ('event_module_block', blocks.EventListBlock()),
             ('newsfeed_block', blocks.NewsFeedModuleBlock()),
+            ('timeline_block', blocks.TimeLineModuleBlock()),
         ],
         null=True,
         blank=True,
--- streams/blocks.py
+++ streams/blocks.py
@@ -699,3 +699,24 @@ class EventListBlock(blocks.StructBlock):
         template = "streams/event_list_block.html"
         icon = "date"
         label = "Event list panel"
+
+
+class TimeLineBlock(blocks.StructBlock):
+    year_field = blocks.CharBlock(required=True, max_length=256)
+    text_editor = blocks.RichTextBlock(required=True)
+    image = ImageChooserBlock(required=True)
+    panel_field = blocks.CharBlock(required=True, max_length=256)
+    employees_field = blocks.CharBlock(required=True, max_length=256)
+    offices_field = blocks.CharBlock(required=True, max_length=256)
+
+
+class TimeLineModuleBlock(blocks.StructBlock):
+
+    timeline = blocks.ListBlock(
+        TimeLineBlock()
+    )
+
+    class Meta:
+        template = "streams/timeline_block.html"
+        icon = "time"
+        label = "Timeline panel"
