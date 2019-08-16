c10b980b621c8be62b2510e661c730c6d7fbac37
YMerritt <53304940+YMerritt@users.noreply.github.com>
2019-08-16 11:50:06 +0200

    quote module fixes (#102)

--- streams/blocks.py
+++ streams/blocks.py
@@ -83,12 +83,12 @@ class LinkTabChooserBlock(blocks.ChoiceBlock):
 
 class QuotationBlock(blocks.StructBlock):
     """Quotation block"""
-    quote_author = blocks.TextBlock(required=True)
-    author_title = blocks.TextBlock(required=True)
+    quote_author = blocks.TextBlock(required=False)
+    author_title = blocks.TextBlock(required=False)
     quote_text = blocks.TextBlock(required=True)
-    link_text = blocks.CharBlock(required=True, max_length=255)
-    link_url = blocks.URLBlock(required=True)
-    link_tab_chooser = LinkTabChooserBlock(required=True, help_text="choose either open image on new or current tab")
+    link_text = blocks.CharBlock(required=False, max_length=255)
+    link_url = blocks.URLBlock(required=False)
+    link_tab_chooser = LinkTabChooserBlock(required=False, help_text="choose either open image on new or current tab")
     optional_padding_above = blocks.BooleanBlock(required=False, help_text="add padding above field")
 
     class Meta:
