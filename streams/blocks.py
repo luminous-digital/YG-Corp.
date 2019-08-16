8f5dd7c553f72f539ced73f8156e44198fdfd6e7
Katarzyna Kucharska <katarzynakucharska@monolynx.com>
2019-08-16 10:51:17 +0200

    text&title module and hero banner fixes

--- streams/blocks.py
+++ streams/blocks.py
@@ -58,8 +58,8 @@ class TitleColorChooserBlock(blocks.ChoiceBlock):
 class TitleAndTextBlock(blocks.StructBlock):
     """Title and text"""
     title = blocks.CharBlock(required=True, help_text='Add your title')
-    text = blocks.TextBlock(required=False, help_text='Add addition text')
     title_colour = TitleColorChooserBlock(required=True)
+    text = blocks.TextBlock(required=False, help_text='Add addition text')
 
     class Meta:
         template = "streams/title_and_text_block.html"
