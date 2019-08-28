b5f9c97dc151c95052b82fea0a8100ff4d37f651
Milewoss <michalwozniak@monolynx.com>
2019-08-28 09:50:26 +0200

    tag fix

--- custom/templatetags/two_column_decor_tag.py
+++ custom/templatetags/two_column_decor_tag.py
@@ -5,7 +5,7 @@ register = template.Library()
 
 @register.filter
 def two_column_decor(first_decor, second_decor):
-    return True if first_decor == second_decor else False
+    return first_decor == second_decor
 
 
 
