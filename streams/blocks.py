6650783e0eb8356e2f88ae4d4db4ad42128aa56b
WozniakMichal <52409805+WozniakMichal@users.noreply.github.com>
2019-09-02 14:13:08 +0200

    static files update for accordion module & global office (#198)

--- streams/blocks.py
+++ streams/blocks.py
@@ -201,6 +201,25 @@ class ImageBlock(blocks.StructBlock):
         label = "Image"
 
 
+class ImageFullBleedHeightChoiceBlock(blocks.ChoiceBlock):
+    HEIGHT_1 = '800'
+    HEIGHT_2 = '600'
+    choices = (
+        (HEIGHT_1, '800px'),
+        (HEIGHT_2, '600px'),
+    )
+
+
+class ImageFullBleedBlock(blocks.StructBlock):
+    image = ImageChooserBlock(required=True, help_text="choose image")
+    image_height = ImageFullBleedHeightChoiceBlock(required=True)
+
+    class Meta:
+        template = 'streams/image_full_bleed_block.html'
+        icon = 'image'
+        label = 'Full Bleed Image'
+
+
 class CalloutsBlock(blocks.StructBlock):
     heading = blocks.CharBlock(required=True, max_length=255)
     callout = blocks.CharBlock(required=True, max_length=255)
