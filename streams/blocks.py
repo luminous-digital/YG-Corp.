0068f61b8db737de7baf2e3da914bd3d3ae0ff26
Milewoss <michalwozniak@monolynx.com>
2019-09-02 17:31:00 +0200

    created Back page link module

--- streams/blocks.py
+++ streams/blocks.py
@@ -1011,3 +1011,28 @@ class PaddingBlock(blocks.StructBlock):
         template = "streams/padding_block.html"
         icon = "arrows-up-down"
         label = "Padding"
+
+
+""" Back Page Link blocks"""
+
+
+class BackPageLinkStyle(blocks.ChoiceBlock):
+    NORMAL = 'Normal'
+    RTE = 'RTE'
+    RTE_FULL = 'RTE_FULL'
+
+    choices = (
+        (NORMAL, 'Normal'),
+        (RTE, 'RTE'),
+        (RTE_FULL, 'RTE FULL'),
+    )
+
+
+class BackPageLinkBlock(blocks.StructBlock):
+    link = blocks.PageChooserBlock(required=True, help_text="choose page")
+    link_style = BackPageLinkStyle(required=True)
+
+    class Meta:
+        template = "streams/back_page_block.html"
+        icon = "site"
+        label = "Back Page panel"
