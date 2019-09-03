a092f711d683ace57c78aff28816c5238a8b0209
WozniakMichal <52409805+WozniakMichal@users.noreply.github.com>
2019-09-03 17:22:45 +0200

    Image person & people blocks and templates updates with css (#207)

--- streams/blocks.py
+++ streams/blocks.py
@@ -366,7 +366,9 @@ class ImagePersonBlock(blocks.StructBlock):
     image = ImageChooserBlock(required=True, help_text="choose image")
     name = blocks.CharBlock(required=True, max_length=255, help_text="add name and surname")
     title = blocks.CharBlock(required=True, max_length=255, help_text="add title")
+    appointed = blocks.DateBlock(required=False)
     biography = blocks.TextBlock(required=True, help_text="biography about")
+    committees = blocks.CharBlock(required=False, max_length=255)
 
     class Meta:
         template = "streams/image_person_block.html"
