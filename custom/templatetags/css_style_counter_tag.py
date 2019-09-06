17725cc9386c061c12f1b9a9f72627e585991362
Milewoss <michalwozniak@monolynx.com>
2019-09-06 16:15:55 +0200

    fixed

--- custom/templatetags/css_style_counter_tag.py
+++ custom/templatetags/css_style_counter_tag.py
@@ -6,5 +6,4 @@ register = template.Library()
 @register.filter
 def css_style_counter(number):
     if number % 4 == 0 or (number - 1) % 4 == 0:
-        return '--alt'
-    return
+        return True
