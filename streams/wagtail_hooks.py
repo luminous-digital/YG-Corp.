398bda54d947d1e2d9368a2e32b9f5d645434c64
Milewoss <michalwozniak@monolynx.com>
2019-09-06 12:53:08 +0200

    RTE modules now opens external link in new tab. Internal links stays the same - the same tab

--- streams/wagtail_hooks.py
+++ streams/wagtail_hooks.py
@@ -147,32 +147,11 @@ def register_righttext_feature(features):
     features.default_features.append(feature_name)
 
 
-def external_linktype_handler(attrs):
+def external_link_type_handler(attrs):
     href = attrs["href"]
     return '<a href="%s" target="_blank">' % escape(href)
 
+
 @hooks.register('register_rich_text_features')
 def register_external_link(features):
-    features.register_link_type('external', external_linktype_handler)
-
-# from django.utils.html import escape
-from wagtail.core import hooks
-from wagtail.core.rich_text import LinkHandler
-
-
-# class NewWindowExternalLinkHandler(LinkHandler):
-#     # This specifies to do this override for external links only.
-#     # Other identifiers are available for other types of links.
-#     identifier = 'external'
-#
-#     @classmethod
-#     def expand_db_attributes(cls, attrs):
-#         href = attrs["href"]
-#         # Let's add the target attr, and also rel="noopener" + noreferrer fallback.
-#         # See https://github.com/whatwg/html/issues/4078.
-#         return '<a href="%s" target="_blank" rel="noopener noreferrer">' % escape(href)
-#
-#
-# @hooks.register('register_rich_text_features')
-# def register_external_link(features):
-#     features.register_link_type(NewWindowExternalLinkHandler)
+    features.register_link_type('external', external_link_type_handler)
