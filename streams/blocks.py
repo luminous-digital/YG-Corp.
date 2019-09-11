aa16cd19cda8aa34e852d971e11683f0d78e0af1
Milewoss <michalwozniak@monolynx.com>
2019-09-11 15:58:52 +0200

    added styles to logos (normal & RTE) if one image it is aligned to left

--- streams/blocks.py
+++ streams/blocks.py
@@ -1098,6 +1098,16 @@ class BackPageLinkBlock(blocks.StructBlock):
 """ Logos module blocks"""
 
 
+class LogoStyleBlock(blocks.ChoiceBlock):
+    NORMAL = 'Normal'
+    RTE = 'RTE'
+
+    choices = (
+        (NORMAL, 'Normal'),
+        (RTE, 'RTE'),
+    )
+
+
 class LogoBlock(blocks.StructBlock):
     logo_image = ImageChooserBlock(required=True, help_text="Choose only svg files")
     logo_alt_text = blocks.CharBlock(required=False, max_length=255, help_text="text that will appear if logo won't show")
@@ -1109,6 +1119,7 @@ class LogosListBlock(blocks.StructBlock):
     logos = blocks.ListBlock(
         LogoBlock()
     )
+    logo_style = LogoStyleBlock(required=False, default=LogoStyleBlock.choices[0],)
 
     class Meta:
         template = "streams/logo_block.html"
