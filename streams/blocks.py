73e64956f2b1cb9f15c10c33dc49542c8232306e
Milewoss <michalwozniak@monolynx.com>
2019-09-13 14:20:00 +0200

    added light grey colour to colour chooser

--- streams/blocks.py
+++ streams/blocks.py
@@ -59,6 +59,7 @@ class TitleColorChooserBlock(blocks.ChoiceBlock):
     VIOLET = '#9078D7'
     ORANGE = '#FF6352'
     GREY = '#605A70'
+    LIGHT_GREY = '#E6E3EC'
     DARK_BLUE_PURPLE = '#241D36'
     BLACK = '#000000'
 
@@ -69,6 +70,7 @@ class TitleColorChooserBlock(blocks.ChoiceBlock):
         (VIOLET, "Violet"),
         (ORANGE, "Orange"),
         (GREY, "Grey"),
+        (LIGHT_GREY, "Light grey"),
         (DARK_BLUE_PURPLE, "Dark blue / Purple"),
         (BLACK, 'Black'),
     )
