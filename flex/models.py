f3ac8a8f7de9a7e982cea5da98a0984639a10482
Milewoss <michalwozniak@monolynx.com>
2019-09-27 15:27:21 +0200

    person_block also commented

--- flex/models.py
+++ flex/models.py
@@ -13,8 +13,9 @@ class AbstractFlexPage(Page):
 
     # commented image_block because of https://luminousweb.atlassian.net/browse/YOUG-16
     # for now it is not necessary and creates misunderstanding with image_content_block
-    # commented accordion_block because of https://luminousweb.atlassian.net/browse/YOUG-234
-    # as a dupliace module to accordion_list_block
+
+    # commented accordion_block and person_block because of https://luminousweb.atlassian.net/browse/YOUG-234
+    # as a duplicate module to accordion_list_block and not used person_block
 
     content = StreamField(
         [
@@ -30,7 +31,7 @@ class AbstractFlexPage(Page):
             ('iframe_block', blocks.IframeBlock()),
             ('image_content_block', blocks.ImageContentBlock()),
             ('image_full_bleed_block', blocks.ImageFullBleedBlock()),
-            ('image_person_block', blocks.ImagePersonBlock()),
+            # ('image_person_block', blocks.ImagePersonBlock()),
             ('image_people_block', blocks.ImagePeopleBlock()),
             ('newsfeed_block', blocks.NewsFeedModuleBlock()),
             ('number_block', blocks.NumberingListBlock()),
